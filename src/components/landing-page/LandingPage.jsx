ec755c1badbccf9b501daf6c33418fe16b0c7ca9
ronakdadwani <dadwanironak456@gmail.com>
2024-12-07 17:23:52 +0530

    music is working fine on the website

--- src/components/landing-page/LandingPage.jsx
+++ src/components/landing-page/LandingPage.jsx
@@ -1,15 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import './LandingPage.css';
+
 
 function LandingPage({ startGame }) {
     const [isReady ,setIsReady] = useState(false);
     const [playerX, setPlayerX] = useState('');
     const [playerO, setPlayerO] = useState('');
+    const audioRef = useRef(null)
 
     useEffect(()=>{
         // simulates css load or dom rediness
         setIsReady(true)
+
+        if (audioRef.current){
+            audioRef.current.volume = 0.2;
+            audioRef.current.play().catch(()=>{
+                console.log("user interaction required to play the music");
+                
+            })
+        }
+
     },[])
+
+    const playMusic = () =>{
+        if(audioRef.current){
+            audioRef.current.play()
+        }
+    }
     const handleStartGame = () => {
         if (playerX && playerO) {
             startGame(playerX, playerO);
@@ -20,6 +37,14 @@ function LandingPage({ startGame }) {
 
     return (
         <div className={`landing-page ${isReady ? "ready" : ''}`}>
+            {/** background music */}
+            <audio ref={audioRef} loop>
+                <source src="./Assets/music/sound1.mp3" type="audio/mpeg" />
+                Your browser dows not support the audio element.
+            </audio>
+            <button onClick={playMusic}>
+                Play Music
+            </button>
             {/* Game Title */}
             <h1 className="welcome-title">Tic Tac Toe</h1>
             
