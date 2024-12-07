c807b01012c6d12e0d9cf1a0a29c2a324ad0e4a6
ronakdadwani <dadwanironak456@gmail.com>
2024-12-07 17:23:16 +0530

    fix the bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css'
 import Grid from './components/Grid/Grid'
-import LandingPage from './components/landing-page/landingpage'
-import backgroundMusic from './components/bg-music/bgmusic'
+
+
 
 
 function App() {
   return (
     <>
-      {/* include ng music */}
-      <backgroundMusic src='./Assets/music/sound1.mp3' /> 
       <Grid numberOfCards={9} />
       
     </>
