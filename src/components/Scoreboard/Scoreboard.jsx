085ce1aba86853f56daeb671f852806618a89757
ronakdadwani <dadwanironak456@gmail.com>
2024-12-25 01:34:14 +0530

    scoreboard.jsx

--- src/components/Scoreboard/Scoreboard.jsx
+++ src/components/Scoreboard/Scoreboard.jsx
@@ -5,12 +5,12 @@ function Scoreboard({playerX , playerO , scoreX , scoreO}) {
     return (
         <div className="scoreboard">
             <div className="player-score">
-                <h2>{playerX || 'Player X'}</h2>
                 <p>Score X : {scoreX}</p>
+                <h2>{playerX || 'Player X'}</h2>
             </div>
             <div className="player-score">
-                <h2> {playerO || 'Player O'} </h2>
                 <p>Score O : {scoreO}</p>
+                <h2> {playerO || 'Player O'} </h2>
             </div>
         </div>
     )
