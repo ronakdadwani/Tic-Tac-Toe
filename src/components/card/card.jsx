af2f115e16fa3eb8072dfb70b522f53a17606961
ronakdadwani <dadwanironak456@gmail.com>
2024-12-28 23:50:01 +0530

    card.jsx

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,35 +1,37 @@
-import isWinner from "../helpers/checkWinner";
 import Icon from "../icon/icon";
-import './Card.css'
+import './Card.css';
 
-function Card({gameEnd , player, onPlay , index}){
-    let icon = <Icon/>
-    if(player === 'X'){
-        icon = <Icon name ="cross"/>
-    } else if (player === 'O'){
-        icon = <Icon name = "circle"/>
+function Card({ gameEnd, player, onPlay, index, clickSoundX, clickSoundO }) {
+    let icon = <Icon />;
+    if (player === 'X') {
+        icon = <Icon name="cross" />;
+    } else if (player === 'O') {
+        icon = <Icon name="circle" />;
     }
 
-    // play muic on card click if it not already playing
-    const playClickSound = () =>{
-       // const soundFile = turn ? '/o-click-sound.mp3' : 'x-click-sound.mp3'; // this line is for 2 sound effect for different different player 
-        const clickSund = new Audio('./Assets/clicksound/click1.mp3')
-        clickSund === isWinner
-        clickSund.play();
-    }
-    // handle card click 
-    const handleClick = ( )=>{
-        if (!gameEnd &&  player === '') {
+    // Play click sound based on the current player
+    const playClickSound = () => {
+        if (player === '') {
+            if (clickSoundX && clickSoundO) {
+                const sound = player === 'O' ? clickSoundO : clickSoundX;
+                sound.play();
+            }
+        }
+    };
+
+    // Handle card click
+    const handleClick = () => {
+        if (!gameEnd && player === '') {
             playClickSound();
             onPlay(index);
         }
-    }
+    };
+
     return (
         <div className="card" onClick={handleClick}>
-        {icon}
-    </div>
-    )
-
+            {icon}
+        </div>
+    );
 }
 
 export default Card;
