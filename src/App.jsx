f574a8fb56f986aa298759d7670bd5a685f6237e
ronakdadwani <dadwanironak456@gmail.com>
2025-06-24 23:27:43 +0530

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 import './App.css'
 import Grid from './components/Grid/Grid'
-
-
+import Login from './components/login/Login';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  if (!user) {
+    return <Login onLogin={setUser} />;
+  }
+
   return (
     <>
-      <Grid numberOfCards={9}/>
+      <Grid numberOfCards={9} user={user} />
     </>
   )
 }
